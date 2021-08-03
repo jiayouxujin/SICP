02c186f431e7f100f0b9e37fc8486cc8493aa280
xujin <1319039722@qq.com>
2021-08-03 11:10:42 +0800

    [exercise] finish exercise 1.4

--- chapter01/exercise11.rkt
+++ chapter01/exercise11.rkt
@@ -74,3 +74,12 @@
         (sum-square y z)))
 )
 (sum-tow-large-number 1 -2 3)
+
+;exercise 1.4
+; (define (a-plus-abs-b a b)
+;     ((if (> b 0)+ -)a b)) if b>0 (+ a b) else (- a b)
+(define (a-plus-abs-b a b)
+    ((if (> b 0)+ -)a b))
+; (a-plus-abs-b 1 2)->3
+; (a-plus-abs-b 1 -2)->3
+
