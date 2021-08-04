7eb74d8964be74a191623cdf6602935f16f801f8
xujin <1319039722@qq.com>
2021-08-04 15:57:25 +0800

    exponentiation

---
+++ chapter01/exercise/exponentiation.rkt
@@ -0,0 +1,45 @@
+#lang sicp
+
+;linear recursive
+(define (expt b n)
+    (if (= n 0)
+        1
+        (* b (expt b (- n 1)))
+    )
+)
+
+(expt 2 1)
+(expt 4 5)
+
+;linear iteration
+(define (expt-it b n)
+    (define (expt-iter res count)
+        (if (= count n)
+            res
+            (expt-iter (* res b) (+ count 1))
+        )
+    )
+    (expt-iter 1 0)
+)
+
+(expt-it 2 1)
+(expt-it 4 5)
+
+;fast-expt
+(define (fast-expt b n)
+    (cond ((= n 0)1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))
+    )
+)
+
+(define (square x)
+    (* x x)
+)
+(define (even? n)
+    (=  (remainder n 2) 0)
+)
+
+
+(fast-expt 2 1)
+(fast-expt 4 5)
