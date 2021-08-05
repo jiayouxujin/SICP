0e246f93c4498b2f16dbf86729f28af5fa50b72a
xujin <1319039722@qq.com>
2021-08-05 18:38:48 +0800

    [exercise] finish exercise 1.36

---
+++ chapter01/high-order-procedures/procedures-as-general-methods/36-prints-the-sequence-of-approximations-it-generates.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define tolerance 0.00001)
+
+(define (display-info guess step)
+    (display "Step: ")
+    (display step)
+    (display " ")
+
+    (display "Guess: ")
+    (display guess)
+    (newline)
+)
+(define (fixed-point f first-guess)
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance)
+    )
+
+    (define (try guess step)
+        (display-info guess step)
+        (let ((next (f guess)))
+            (if (close-enough? guess next)
+                next
+                (try next (+ 1 step))
+            )
+        )
+    )
+    (try first-guess 1)
+)
+
+(fixed-point (lambda(x)(/ (log 1000) (log x))) 2.0)
