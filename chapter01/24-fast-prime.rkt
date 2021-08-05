b1fc4d600069a3509cc8ad72b0b928746e8aba6f
xujin <1319039722@qq.com>
2021-08-05 11:14:01 +0800

    [exercise] finish exercise 1.24

---
+++ chapter01/24-fast-prime.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+(#%require "../lib.scm")
+
+
+(define (expmod base exp m)
+    (cond ((= exp 0) 1)
+        ((even? exp)
+            (remainder (square (expmod base (/ exp 2) m)) m)
+        )
+        (else
+        (remainder (* base (expmod base (- exp 1) m)) m))
+    )
+)
+
+(define (fermat-test n)
+    (define (try-it a)
+        (= (expmod a n n) a)
+    )
+    (try-it (+ 1 (random (- n 1))))
+)
+
+(define (fast-prime? n time)
+    (cond ((= time 0) true)
+        ((fermat-test n) (fast-prime? n (- time 1)))
+        (else false)
+    )
+)
+
+(define (timed-prime-test n)
+    (define start-time (runtime))
+    (display (fast-prime? n 5))
+    (- (runtime) start-time)
+)
+
+(timed-prime-test 2)
