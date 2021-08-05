f8abb6e65c4808ca3ca7871cd8c2393664877c53
xujin <1319039722@qq.com>
2021-08-05 21:39:52 +0800

    average-damp

---
+++ chapter01/high-order-procedures/procedures-as-returned-values/average-damp.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+(#%require "../procedures-as-general-methods/finding-fixed-points-of-functions.rkt")
+
+(define average (lambda(x y) (/ (+ x y) 2)))
+
+(define (average-damp f)
+    (lambda(x) (average x (f x)))
+)
+
+((average-damp (lambda(x) (* x x))) 10)
+
+(define (sqrt x)
+    (fixed-point (average-damp (lambda(y) (/ x y))) 1.0)
+)
+
+(sqrt 4)
+
+(define square (lambda(x) (* x x)))
+(define (cube-root x)
+    (fixed-point (average-damp (lambda(y)(/ x (square y)))) 1.0)
+)
+
+(define (deriv g)
+    (lambda(x) (/ (- (g (+ x dx)) (g x)) dx))
+)
+(define dx 0.00001)
+((deriv (lambda(x)(* x x x))) 5)
