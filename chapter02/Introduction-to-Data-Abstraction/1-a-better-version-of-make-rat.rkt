4bc61b5156722d76836f04cd881d6c90d7eedada
xujin <1319039722@qq.com>
2021-08-06 11:10:09 +0800

    [exercise] finish exercise 2.1

---
+++ chapter02/Introduction-to-Data-Abstraction/1-a-better-version-of-make-rat.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+
+;exercise 2.1
+
+(define (make-rat n d)
+   (if (< d 0)
+    (cons (- n) (- d))
+    (cons n d)
+   )
+)
+
+(define (numer x)
+    (car x)
+)
+
+(define (denom x)
+    (cdr x)
+)
+
+(define (print-rat x)
+    (newline)
+    (display (numer x))
+    (display "/")
+    (display (denom x))
+)
+
+
+(print-rat (make-rat -1 2))
+(print-rat (make-rat -1 -2))
