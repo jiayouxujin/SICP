62f06d7a32c08316a5c6c23bdc2e3caccb652045
xujin <1319039722@qq.com>
2021-08-07 22:50:55 +0800

    [exercise] update exercise 2.29

--- chapter02/Hierarchical-data-and-the-closure-property/29-tow-branches.rkt
+++ chapter02/Hierarchical-data-and-the-closure-property/29-tow-branches.rkt
@@ -3,21 +3,29 @@
 
 ;exercise 2.29
 
+; (define (make-mobile left right)
+;     (list left right)
+; )
+
+; (define (make-branch length structure)
+;     (list length structure)
+; )
+
+;d
 (define (make-mobile left right)
-    (list left right)
+    (cons left right)
 )
 
 (define (make-branch length structure)
-    (list length structure)
+    (cons length structure)
 )
-
-;a
+; ;a
 (define (left-branch x)
     (car x)
 )
 
 (define (right-branch x)
-    (cadr x)
+    (cdr x)
 )
 
 (define (branch-length x)
@@ -25,7 +33,7 @@
 )
 
 (define (branch-structure x)
-    (cadr x)
+    (cdr x)
 )
 
 
@@ -45,6 +53,7 @@
 ;     (pair? (branch-structure branch))
 ; )
 
+;b
 (define (total-weight z)
     (cond ((null? z) 0)
         ((not (pair? (branch-structure z)))
@@ -54,5 +63,29 @@
     )
 )
 
-(define mobile (make-mobile (make-branch 10 20) (make-branch 10 25)))
+(define mobile (make-mobile (make-branch 10 20) (make-branch 20 10)))
 (total-weight mobile)
+
+;c
+(define (balanced? mobile)
+    (let((left (left-branch mobile))
+        (right (right-branch mobile)))
+        (and (same-torque? left right) (branch-balance? left) (branch-balance? right))
+    )
+)
+
+(define (branch-torque branch)
+    (* (branch-length branch) (total-weight branch))
+)
+(define (same-torque? left right)
+    (= (branch-torque left) (branch-torque right))
+)
+
+(define (branch-balance? branch)
+    (if (pair? (branch-structure branch))
+        (balanced? (branch-structure branch))
+        true    
+    )
+)
+(balanced? mobile)
+
