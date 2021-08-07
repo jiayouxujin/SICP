f1c0952826c417d459b6f02ce89d7a90ce236e56
xujin <1319039722@qq.com>
2021-08-07 18:04:39 +0800

    [exercise] finish exercise 2.22

---
+++ chapter02/Hierarchical-data-and-the-closure-property/22-louis.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define square (lambda(x) (* x x)))
+; (define (square-list items)
+;     (define (iter things answer)
+;         (if (null? things)
+;             answer
+;             (iter (cdr things) (cons (square (car things)) answer))
+;         )
+;     )
+;     (iter items nil)
+; )
+
+
+(define (square-list items)
+    (define (iter things answer)
+        (if (null? things)
+            answer
+            (iter (cdr things) (cons answer (square (car things))))
+        )
+    )
+    (iter items nil)
+)
+(square-list (list 1 2 3 4))
