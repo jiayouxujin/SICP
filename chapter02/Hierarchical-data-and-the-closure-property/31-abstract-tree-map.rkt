5e9224947fe78f84153b8879be4076e2693f66e4
xujin <1319039722@qq.com>
2021-08-07 23:11:56 +0800

    [exercise] finish exercise 2.31

---
+++ chapter02/Hierarchical-data-and-the-closure-property/31-abstract-tree-map.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+;exercise 2.31
+
+(define (map f items)
+    (if (null? items)
+        nil
+        (cons (f (car items)) (map f (cdr items)))
+    )
+)
+(define (tree-map f tree)
+    (map(lambda(sub-tree)
+        (if (pair? sub-tree)
+            (tree-map f sub-tree)
+            (f sub-tree)
+        )
+    )
+    tree
+    )
+)
+
+(define (square-tree tree)
+    (tree-map (lambda(x)(* x x)) tree)
+)
+
+
+(square-tree
+    (list 1
+        (list 2 (list 3 4) 5 )
+            (list 6 7)))
