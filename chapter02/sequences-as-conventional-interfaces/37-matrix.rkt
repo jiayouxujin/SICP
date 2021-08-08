86d31a3711e604e6711b78dcc8147a0c9afb4092
xujin <1319039722@qq.com>
2021-08-08 14:18:26 +0800

    [exercise] finish exercise 2.37

---
+++ chapter02/sequences-as-conventional-interfaces/37-matrix.rkt
@@ -0,0 +1,45 @@
+#lang sicp
+
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence) (accumulate op initial (cdr sequence)))
+    )
+)
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    nil
+    (cons (accumulate op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+;exercise 2.37
+(define (dot-product v w)
+    (accumulate + 0 (map * v w))
+)
+
+(define (matrix-*-vector m v)
+    (map (lambda(col) (dot-product v col)) m)
+)
+
+(define m (list(list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+(define v (list 1 2 3 4))
+(matrix-*-vector m v)
+
+(define (transpose mat)
+    (accumulate-n cons nil mat)
+)
+(transpose m)
+
+; (define (matrix-*-matrix m n)
+;   (let ((cols (transpose n)))
+;     (map (lambda(row) (matrix-*-vector cols row)) m)))
+
+(define (matrix-*-matrix m n)
+    (let ((cols (transpose n)))
+        (map (lambda(col-of-m)(map (lambda(col-of-cols)(dot-product col-of-m col-of-cols)) cols))m)
+    )
+)
+
+(matrix-*-matrix m (transpose m))
