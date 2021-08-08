181a7e622ab0530a2e6819a4755e7b80b80cc683
xujin <1319039722@qq.com>
2021-08-08 08:56:31 +0800

    sum odd squares

---
+++ chapter02/sequences-as-conventional-interfaces/sum-odd-squares.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+
+(define (square x)
+    (* x x)
+)
+(define (sum-odd-squares tree)
+    (cond ((null? tree) 0)
+        ((not (pair? tree))
+            (if (odd? tree) (square tree) 0)
+        )
+        (else (+ (sum-odd-squares (car tree)) (sum-odd-squares (cdr tree))))
+    )
+)
