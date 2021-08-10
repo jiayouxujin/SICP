91290ab9f6b4af9834e33948a43300c9e7fd2085
xujin <1319039722@qq.com>
2021-08-10 09:28:46 +0800

    [exercise] finish exercise 2.66

---
+++ chapter02/Symbolic-Data/2-66.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+;exercise 2.66
+
+(define (lookup-tree given-key set-of-records)
+    (cond ((null? set-of-records) false)
+        ((= given-key (key (entry set-of-records))) (entry set-of-records))
+        ((> given-key (key (entry set-of-records))) 
+            (lookup-tree given-key (right-branch set-of-records))
+        )
+        (else(lookup-tree given-key (left-branch set-of-records)))
+    )
+)
