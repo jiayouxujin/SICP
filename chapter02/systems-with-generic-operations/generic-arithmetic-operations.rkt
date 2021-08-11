95ff4e1c19c32ee96614013c112a8c58fcaca773
xujin <1319039722@qq.com>
2021-08-11 10:42:59 +0800

    complex number

--- chapter02/systems-with-generic-operations/generic-arithmetic-operations.rkt
+++ chapter02/systems-with-generic-operations/generic-arithmetic-operations.rkt
@@ -21,6 +21,10 @@
           (else (get-helper k (cdr array)))))
   (get-helper (list op type) global-array))
 
+
+(define (square x)
+    (* x x)
+)
 (define (attach-tag type-tag . contents)
     (cons type-tag contents)
 )
@@ -133,3 +137,114 @@
 (define rational-1 (make-rational 1 2))
 (define rational-2 (make-rational 3 4))
 (add rational-1 rational-2)
+
+;rectangular-package
+(define (install-rectangular-package)
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (make-from-real-imag x y) (cons x y))
+    (define (magnitude z)
+        (sqrt (+ (square (real-part z)) (square (imag-part z))))
+    )
+    (define (angle z)
+        (atan (imag-part z) (real-part z))
+    )
+
+    (define (make-from-mag-ang r a)
+        (cons (* r (cos a)) (* r (sin a)))
+    )
+
+    (define (tag x) (attach-tag 'rectangular x))
+    (put 'real-part '(rectangular) real-part)
+    (put 'imag-part '(rectangular) imag-part)
+    (put 'magnitude '(rectangular) magnitude)
+    (put 'angle '(rectangular) angle)
+
+    (put 'make-from-real-imag 'rectangular
+        (lambda(x y) (tag (make-from-real-imag x y))))
+
+    (put 'make-from-mag-ang 'rectangular
+        (lambda(x y)(tag (make-from-mag-ang x y))))
+
+'done)
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+(install-rectangular-package)
+
+
+(define (install-complex-package)
+    (define (make-from-real-imag x y)
+        ((get 'make-from-real-imag 'rectangular) x y)   
+    )
+    (define (make-from-mag-ang r a)
+        ((get 'make-from-mag-ang 'polar) r a)
+    )
+
+    (define (add-complex z1 z2)
+        (make-from-real-imag (+(real-part z1) (real-part z2)) (+ (imag-part z1) (imag-part z2)))
+    )
+
+    (define (sub-complex z1 z2)
+        (make-from-real-imag (- (real-part z1) (real-part z2)) (- (imag-part z1) (imag-part z2)))
+    )
+
+    (define (mul-complex z1 z2)
+        (make-from-mag-ang (* (magnitude z1) (magnitude z2)) (- (angle z1) (angle z2)))
+    )
+
+    (define (div-complex z1 z2)
+        (make-from-mag-ang (/ (magnitude z1) (magnitude z2)) (- (angle z1) (angle z2)))
+    )
+
+    (define (tag z) (attach-tag 'complex z))
+    
+    (put 'real-part '(complex) real-part)
+    (put 'imag-part '(complex) imag-part)
+    (put 'magnitude '(complex) magnitude)
+    (put 'angle '(complex) angle)    
+
+    (put 'add '(complex complex)
+        (lambda(z1 z2) (tag (add-complex z1 z2)))
+    )
+
+    (put 'sub '(complex complex)
+        (lambda(z1 z2) (tag (sub-complex z1 z2)))
+    )
+
+    (put 'mul '(complex complex)
+        (lambda(z1 z2) (tag (mul-complex z1 z2)))
+    )
+
+    (put 'div '(complex complex)
+        (lambda(z1 z2) (tag (div-complex z1 z2)))
+    )
+
+    (put 'make-from-real-imag 'complex
+        (lambda(x y) (tag (make-from-real-imag x y)))
+    )
+
+    (put 'make-from-mag-ang 'complex
+        (lambda(r a) (tag (make-from-mag-ang r a))))
+'done)
+
+(define (make-complex-from-real-imag x y)
+    ((get 'make-from-real-imag 'complex) x y)
+)
+
+(define (make-complex-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'complex) r a)
+)
+
+(install-complex-package)
+; (get 'make-from-real-imag 'complex)
+(define complex-1 (make-complex-from-real-imag 1 2))
+(define complex-2 (make-complex-from-real-imag 3 4))
+(real-part complex-1)
+(real-part complex-2)
+(imag-part complex-1)
+(imag-part complex-2)
+(add complex-1 complex-2)
+(magnitude complex-1)
