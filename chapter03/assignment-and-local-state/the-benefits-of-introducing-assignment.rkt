2f40319621ef7917484917dc7cef97c04e15ca25
xujin <1319039722@qq.com>
2021-08-12 18:44:44 +0800

    the benefit of assignment

---
+++ chapter03/assignment-and-local-state/the-benefits-of-introducing-assignment.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define rand
+    (let ((x random-init))
+        (lambda()
+            (set! x (rand-update x))
+        x)
+    )
+)
+(define (cesaro-test)
+    (= (gcd (rand) (rand)) 1)
+)
+
+(define (monte-carlo trails experiment)
+    (define (iter trials-remaining trials-passed)
+        (cond ((= trials-remaining 0)
+                (/ trials-passed trials))
+            ((experiment)
+                (iter (- trials-remaining 1)
+                    (+ trials-passed 1)
+                )
+            )
+            (else
+                (iter (- trials-remaining 1) trials-passed))
+        )
+    )
+    (iter trials 0)
+)
+
+(define (estimate-pi trials)
+    (sqrt (/ 6 (monte-carlo trials cesaro-test)))
+)
