5b591babc06d7cf664c410c608f1d78563e08c66
xujin <1319039722@qq.com>
2021-08-13 15:08:53 +0800

    [exercise] finish exercise 3.14

---
+++ chapter03/3-3/3-14.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+
+;exercise 3.14
+(define (mystery x)
+    (define (loop x y)
+        (if (null? x)
+            y
+            (let ((temp (cdr x)))
+                (set-cdr! x y)
+                (loop temp x)
+            )
+        )
+    )
+    (loop x '())
+)
+
+(define v (list 'a 'b 'c 'd))
+v
+(define w (mystery v))
+w
